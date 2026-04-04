49ef7751e0f244b8594653e827a2f7e2ad17d64c
Mahmoud <mahmoudreda2020outlooc@gmail.com>
2026-04-04 17:48:08 +0200

    إصلاح نهائي وتخطي الكاش (Renaming SW) 📲🚀🔥

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
             __html: `
               if ('serviceWorker' in navigator) {
                 window.addEventListener('load', function() {
-                  navigator.serviceWorker.register('/sw.js');
+                  navigator.serviceWorker.register('/sw-v4.js');
                 });
               }
             `,
