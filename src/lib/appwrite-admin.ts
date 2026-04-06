c514df787dfc61eb0aca5c78854e3135145b7e05
Mahmoud <mahmoudreda2020outlooc@gmail.com>
2026-04-06 15:43:57 +0200

    fix(auth): hardcode admin api key to fix server validation error

--- src/lib/appwrite-admin.ts
+++ src/lib/appwrite-admin.ts
@@ -4,7 +4,7 @@ export function createAdminClient() {
     const client = new Client()
         .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1')
         .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '69662d2200214465b1d3')
-        .setKey(process.env.APPWRITE_API_KEY || ''); // مفتاح السيرفر (Admin API Key)
+        .setKey(process.env.APPWRITE_API_KEY || 'standard_96dc2441291ebaecb390aa6768280e280a85805c7c81317d7a82583ae1beb812e644475c368aed8bde042a2dd45a516f1ac67bfd371caa4fcba56d8412466a6551155e623f44eb6c9953957affb48eaea28e3a0f14583a626f613b46083fa09466715153b2c3fd4ae7b24f8573d245b9c86eb87159d38817fe3b491451b55390'); // مفتاح السيرفر (Admin API Key)
 
     return {
         get account() { return new Account(client); },
